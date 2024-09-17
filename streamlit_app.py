74f40d580461bc6294448f9904fc81fe6b8afa55
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-16 21:04:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,7 @@ selected_requestType = st.selectbox(
    "Type of Request",
    ("Grant Functional Role(s) to a Project Role", "Grant Functional/Project Role(s) to a Service Role", "Revoke Functional Role(s) from a Project Role", "Grant Functional/Project Role(s) from a Service Role"),
    index=None,
+   key='reqtyp'
 )
 
 
