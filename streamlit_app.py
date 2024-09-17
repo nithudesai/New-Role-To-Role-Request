4a07212351c098d8cfb8114410b8dc4b7c27c5c5
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-16 22:48:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,7 @@ with st.form("form1", clear_on_submit = True):
     # print form responses
     if submit:
 
-        formResponses = "Environment:" + str(selected_environment) + "\n Type of Request:" + str(selected_requestType) + "\n Selected Source Roles:" + str(Selected_Source_Values)[1:-1] + "\n Selected Target Roles:" + "\n str(Selected_Target_Values)" + "\n Reason for Request:" + str(reasonForRequest) 
+        formResponses = "Environment:" + str(selected_environment) + "  \n Type of Request:" + str(selected_requestType) + "  \n Selected Source Roles:" + str(Selected_Source_Values)[1:-1] + "  \n Selected Target Roles:" + " str(Selected_Target_Values)" + "  \n Reason for Request:" + str(reasonForRequest) 
         st.header('Form Responses')
 
         st.write(formResponses)
