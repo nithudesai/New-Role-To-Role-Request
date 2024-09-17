8b86c7ffec80e8314fd55b681e6d9d3d0b41f422
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-17 17:13:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,10 +178,10 @@ with st.form("form1", clear_on_submit = True):
     if submit:
 
         #formResponses="Environment:" + str(selected_environment) + "  \n Type of Request:" + str(selected_requestType) + "  \n Selected Source Roles:" + str(Selected_Source_Values)[1:-1] + "  \n Selected Target Roles:" +  str(Selected_Target_Values) + "  \n Reason for Request:" + str(reasonForRequest) 
-        formResponses= """" + "Environment: " + """" + "" " + str(selected_environment) + "" " 
+        formResponses= """" + "Environment: " + """" + """" + str(selected_environment) + """" 
         #+ "  \n Type of Request:" + str(selected_requestType) + "  \n Selected Source Roles:" + str(Selected_Source_Values)[1:-1] 
         #                + "  \n Selected Target Roles:" +  str(Selected_Target_Values) + "  \n Reason for Request:" + str(reasonForRequest) 
-        
+        st.write(formResponses)
         # open snowflake connection
         conn = snowflake.connector.connect(**st.secrets["snowflake"])
 
