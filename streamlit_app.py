2f7e829b644cf547989d05ffc5179bfba50dcc03
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-16 21:47:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ conn = snowflake.connector.connect(**st.secrets["snowflake"])
 
 # populate dropdown values from SF queries - TODO insert more queries
 
+# Dev Roles
 sql = "select name from FR_ROLES where name ilike '%dev%' UNION SELECT 'OTHER' ORDER BY 1 "
 Dev_Func_Roles_Values = get_sf_dropdown_values(sql)
 
@@ -24,18 +25,31 @@ Dev_FR_PR_Values = get_sf_dropdown_values(sql)
 sql = "select name from SVC_ROLES  where name ilike '%dev%'  UNION SELECT 'OTHER' ORDER BY 1 "
 Dev_Svc_Roles_Values = get_sf_dropdown_values(sql)
 
+# Tst Roles
+sql = "select name from FR_ROLES where name ilike '%tst%'  UNION SELECT 'OTHER' ORDER BY 1 "
+Tst_Func_Roles_Values = get_sf_dropdown_values(sql)
 
-sql = "select name from FR_ROLES UNION SELECT 'OTHER' ORDER BY 1 "
-Func_Roles_Values = get_sf_dropdown_values(sql)
+sql = "select name from PRJ_ROLES  where name ilike '%tst%' ORDER BY 1"
+Tst_Prj_Roles_Values = get_sf_dropdown_values(sql)
 
-sql = "select name from PRJ_ROLES ORDER BY 1"
-Prj_Roles_Values = get_sf_dropdown_values(sql)
+sql = "select name from PRJ_ROLES   where name ilike '%tst%' UNION SELECT name from FR_ROLES  where name ilike '%tst%' UNION SELECT 'OTHER' ORDER BY 1 "
+Tst_FR_PR_Values = get_sf_dropdown_values(sql)
 
-sql = "select name from PRJ_ROLES UNION SELECT name from FR_ROLES  UNION SELECT 'OTHER' ORDER BY 1 "
-FR_PR_Values = get_sf_dropdown_values(sql)
+sql = "select name from SVC_ROLES where name ilike '%tst%' UNION SELECT 'OTHER' ORDER BY 1 "
+Tst_Svc_Roles_Values = get_sf_dropdown_values(sql)
 
-sql = "select name from SVC_ROLES  UNION SELECT 'OTHER' ORDER BY 1 "
-Svc_Roles_Values = get_sf_dropdown_values(sql)
+# Prod Roles
+sql = "select name from FR_ROLES where name ilike '%prd%'  UNION SELECT 'OTHER' ORDER BY 1 "
+Prd_Func_Roles_Values = get_sf_dropdown_values(sql)
+
+sql = "select name from PRJ_ROLES  where name ilike '%prd%' ORDER BY 1"
+Prd_Prj_Roles_Values = get_sf_dropdown_values(sql)
+
+sql = "select name from PRJ_ROLES   where name ilike '%prd%' UNION SELECT name from FR_ROLES  where name ilike '%prd%' UNION SELECT 'OTHER' ORDER BY 1 "
+Prd_FR_PR_Values = get_sf_dropdown_values(sql)
+
+sql = "select name from SVC_ROLES where name ilike '%prd%' UNION SELECT 'OTHER' ORDER BY 1 "
+Prd_Svc_Roles_Values = get_sf_dropdown_values(sql)
 
 # close snowflake connection
 conn.close()
@@ -82,13 +96,65 @@ with st.form("form1", clear_on_submit = True):
           FrPrRoleValues = col3.multiselect(
           "Choose functional/project role(s)",
           (Dev_FR_PR_Values),
-          )
-	  
+          )	  
           SvcRoleValues = col4.selectbox(
           "Choose a target Service Acct role",
           (Dev_Svc_Roles_Values),
           index=None,
           )
+    elif selected_environment == 'TST':
+       if selected_requestType == 'Grant Functional Role(s) to a Project Role':
+          FrRoleValues = col1.multiselect(
+          "Choose functional role(s)",
+          (Tst_Func_Roles_Values),
+          placeholder="roles you'd like to add to target project role",
+          help="Choose functional roles you'd like to add to your project role"
+          )
+          PrjRoleValues = col2.selectbox(
+          "Choose a target Project role",
+          (Tst_Prj_Roles_Values),
+          index=None,
+          placeholder="role you'd like to add the additional access",
+          help="Choose a target project tole that you'd like to add the additional access"
+          )
+       elif selected_requestType == 'Grant Functional/Project Role(s) to a Service Role':
+          col3, col4 = st.columns(2)
+          FrPrRoleValues = col3.multiselect(
+          "Choose functional/project role(s)",
+          (Tst_FR_PR_Values),
+          )	  
+          SvcRoleValues = col4.selectbox(
+          "Choose a target Service Acct role",
+          (Tst_Svc_Roles_Values),
+          index=None,
+          )
+    elif selected_environment == 'PRD':
+       if selected_requestType == 'Grant Functional Role(s) to a Project Role':
+          FrRoleValues = col1.multiselect(
+          "Choose functional role(s)",
+          (Prd_Func_Roles_Values),
+          placeholder="roles you'd like to add to target project role",
+          help="Choose functional roles you'd like to add to your project role"
+          )
+          PrjRoleValues = col2.selectbox(
+          "Choose a target Project role",
+          (Prd_Prj_Roles_Values),
+          index=None,
+          placeholder="role you'd like to add the additional access",
+          help="Choose a target project tole that you'd like to add the additional access"
+          )
+       elif selected_requestType == 'Grant Functional/Project Role(s) to a Service Role':
+          col3, col4 = st.columns(2)
+          FrPrRoleValues = col3.multiselect(
+          "Choose functional/project role(s)",
+          (Prd_FR_PR_Values),
+          )	  
+          SvcRoleValues = col4.selectbox(
+          "Choose a target Service Acct role",
+          (Prd_Svc_Roles_Values),
+          index=None,
+          )
+
 
     reasonForRequest = st.text_area(
         "Reason for Request",
@@ -101,7 +167,6 @@ with st.form("form1", clear_on_submit = True):
     # print form responses
     if submit:
         st.header('Form Responses')
-        st.write("Snowflake Account: ", snowflakeAccount)
         st.write("Environment(s): ", environments)
         st.write("Type of Request: ", requestType)
         # TODO add role options 
