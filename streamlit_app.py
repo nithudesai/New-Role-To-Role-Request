1d3c8646b9f538959f4a8ef9a6255589cdb2fedd
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-21 21:25:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,4 +227,4 @@ if submit:
 
     # create button to Download form response
     st.header('Download Request Responses')
-    st.download_button("Download File", str(formResponses),file_name= "Request_"+str(formId)+"_"+".txt")
+    st.download_button("Download File", str(formResponses),file_name= "Request_"+str(formId)+".txt")
