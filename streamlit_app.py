f6c4fde3a263c0b734a990dd20c26a639cf2a668
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-25 10:02:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,30 +88,34 @@ with st.form("form1", clear_on_submit = True):
 
     if selected_environment == 'DEV':
        if selected_requestType == 'Grant Functional Role(s) to a Project Role':
-          Selected_Source_Values = col1.multiselect(
-          "Choose functional role(s)",
-          (Dev_Func_Roles_Values),
-          placeholder="roles you'd like to add to target project role",
-          help="Choose functional roles you'd like to add to your project role"
-          )
-          Selected_Target_Values = col2.selectbox(
+          
+          Selected_Target_Value = col1.selectbox(
           "Choose a target Project role",
           (Dev_Prj_Roles_Values),
           index=None,
           placeholder="role you'd like to add the additional access",
           help="Choose a target project tole that you'd like to add the additional access"
           )
+           
+          Selected_Source_Values = col2.multiselect(
+          "Choose functional role(s)",
+          (Dev_Func_Roles_Values),
+          placeholder="roles you'd like to add to target project role",
+          help="Choose functional roles you'd like to add to your project role"
+          )
+
        elif selected_requestType == 'Grant Functional/Project Role(s) to a Service Role':
           col3, col4 = st.columns(2)
-          Selected_Source_Values = col3.multiselect(
-          "Choose functional/project role(s)",
-          (Dev_FR_PR_Values),
-          )	  
-          Selected_Target_Values = col4.selectbox(
+          Selected_Target_Value = col3.selectbox(
           "Choose a target Service Acct role",
           (Dev_Svc_Roles_Values),
           index=None,
           )
+          Selected_Source_Values = col4.multiselect(
+          "Choose functional/project role(s)",
+          (Dev_FR_PR_Values),
+          )	  
+
     elif selected_environment == 'TST':
        if selected_requestType == 'Grant Functional Role(s) to a Project Role':
           Selected_Source_Values = col1.multiselect(
@@ -120,7 +124,7 @@ with st.form("form1", clear_on_submit = True):
           placeholder="roles you'd like to add to target project role",
           help="Choose functional roles you'd like to add to your project role"
           )
-          Selected_Target_Values = col2.selectbox(
+          Selected_Target_Value = col2.selectbox(
           "Choose a target Project role",
           (Tst_Prj_Roles_Values),
           index=None,
@@ -133,7 +137,7 @@ with st.form("form1", clear_on_submit = True):
           "Choose functional/project role(s)",
           (Tst_FR_PR_Values),
           )	  
-          Selected_Target_Values = col4.selectbox(
+          Selected_Target_Value = col4.selectbox(
           "Choose a target Service Acct role",
           (Tst_Svc_Roles_Values),
           index=None,
@@ -146,7 +150,7 @@ with st.form("form1", clear_on_submit = True):
           placeholder="roles you'd like to add to target project role",
           help="Choose functional roles you'd like to add to your project role"
           )
-          Selected_Target_Values = col2.selectbox(
+          Selected_Target_Value = col2.selectbox(
           "Choose a target Project role",
           (Prd_Prj_Roles_Values),
           index=None,
@@ -159,7 +163,7 @@ with st.form("form1", clear_on_submit = True):
           "Choose functional/project role(s)",
           (Prd_FR_PR_Values),
           )	  
-          Selected_Target_Values = col4.selectbox(
+          Selected_Target_Value = col4.selectbox(
           "Choose a target Service Acct role",
           (Prd_Svc_Roles_Values),
           index=None,
@@ -181,11 +185,11 @@ if submit:
     #formResponses="Environment:" + str(selected_environment) + "  \n Type of Request:" + str(selected_requestType) + "  \n Selected Source Roles:" + str(Selected_Source_Values)[1:-1] + "  \n Selected Target Roles:" +  str(Selected_Target_Values) + "  \n Reason for Request:" + str(reasonForRequest) 
     #formResponses= "\"" + "Environment " + "\":" + "\"" + str(selected_environment) + "\"" 
     #+ "  \n Type of Request:" + str(selected_requestType) + "  \n Selected Source Roles:" + str(Selected_Source_Values)[1:-1] 
-    #                + "  \n Selected Target Roles:" +  str(Selected_Target_Values) + "  \n Reason for Request:" + str(reasonForRequest) 
+    #                + "  \n Selected Target Roles:" +  str(Selected_Target_Value) + "  \n Reason for Request:" + str(reasonForRequest) 
     
     #formResponses = { "selected_environment" : selected_environment , "selected_requestType" : selected_requestType }
         
-    formResponses = { "Environment" : selected_environment , "Type of Request" : selected_requestType , "Selected Source Roles" : Selected_Source_Values , "Selected Target Roles" : Selected_Target_Values ,  "Reason for Request" : reasonForRequest }
+    formResponses = { "Environment" : selected_environment , "Type of Request" : selected_requestType , "Selected Source Roles" : Selected_Source_Values , "Selected Target Roles" : Selected_Target_Value ,  "Reason for Request" : reasonForRequest }
     formResponsesStr = json.dumps(formResponses)
 
     # open snowflake connection
@@ -222,7 +226,7 @@ if submit:
     #st.write("Environment(s): ", selected_environment)
     #st.write("Type of Request: ", selected_requestType)
     #st.write("Selected Source Roles: ", str(Selected_Source_Values))
-    #st.write("Selected Target Roles: ", Selected_Target_Values)
+    #st.write("Selected Target Roles: ", Selected_Target_Value)
     #st.write("Reason for Request: ", reasonForRequest)
 
     # create button to Download form response
